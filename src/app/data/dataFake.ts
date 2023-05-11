64f383468039964909415a788814a46cebe1c3b0
Ana Paula Gatti <100304320+Ana-Gatti@users.noreply.github.com>
2023-05-11 00:44:47 -0300

    Subindo modificações realizadas

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,64 @@
+import { getLocaleEraNames } from "@angular/common";
+
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "ChatGPT e o impacto da inteligência artificial em nossas vidas",
+    "description": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Integer ullamcorper mi sit amet velit feugiat aliquet tempus vitae velit. 
+    Ut pellentesque, nulla sed gravida bibendum, velit elit congue sem, eu faucibus 
+    lorem libero non erat. Sed accumsan nunc vel mi maximus euismod. Vivamus feugiat nunc lacus. 
+    Donec id velit quis nulla sodales varius vestibulum at elit. Etiam consequat nec tellus 
+    et convallis. Nulla sit amet erat in lectus pretium rhoncus ut nec tortor. 
+    Pellentesque pretium magna ac dignissim posuere. Fusce in nisl vehicula, luctus purus ut, 
+    varius nisl. Quisque vestibulum odio bibendum elit tempus, sed tempus nunc rutrum. 
+    Donec finibus finibus eros at porttitor. Nulla pellentesque eros finibus aliquet lacinia.
+    Suspendisse dignissim sollicitudin leo, sed convallis neque sollicitudin `,
+    "photoCover":"https://www.mundodomarketing.com.br/wp-content/uploads/2023/05/Como-a-Inteligencia-Artificial-esta-revolucionando-a-economia-Especial-Web-Summit-Rio-2023.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
+    "title": "Midjourney: imagens mostram evolução da IA em apenas um ano",
+    "description": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Integer ullamcorper mi sit amet velit feugiat aliquet tempus vitae velit. 
+    Ut pellentesque, nulla sed gravida bibendum, velit elit congue sem, eu faucibus 
+    lorem libero non erat. Sed accumsan nunc vel mi maximus euismod. Vivamus feugiat nunc lacus. 
+    Donec id velit quis nulla sodales varius vestibulum at elit. Etiam consequat nec tellus 
+    et convallis. Nulla sit amet erat in lectus pretium rhoncus ut nec tortor. 
+    Pellentesque pretium magna ac dignissim posuere. Fusce in nisl vehicula, luctus purus ut, 
+    varius nisl. Quisque vestibulum odio bibendum elit tempus, sed tempus nunc rutrum. 
+    Donec finibus finibus eros at porttitor. Nulla pellentesque eros finibus aliquet lacinia.
+    Suspendisse dignissim sollicitudin leo, sed convallis neque sollicitudin `,
+    "photoCover":"https://tm.ibxk.com.br/2023/04/27/27085415246041.jpg?ims=1120x420"
+  },
+  {
+    "id":"3",
+    "title": "Um chatbot para tudo (e todos). O que está por detrás da explosão de bots com IA?",
+    "description": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Integer ullamcorper mi sit amet velit feugiat aliquet tempus vitae velit. 
+    Ut pellentesque, nulla sed gravida bibendum, velit elit congue sem, eu faucibus 
+    lorem libero non erat. Sed accumsan nunc vel mi maximus euismod. Vivamus feugiat nunc lacus. 
+    Donec id velit quis nulla sodales varius vestibulum at elit. Etiam consequat nec tellus 
+    et convallis. Nulla sit amet erat in lectus pretium rhoncus ut nec tortor. 
+    Pellentesque pretium magna ac dignissim posuere. Fusce in nisl vehicula, luctus purus ut, 
+    varius nisl. Quisque vestibulum odio bibendum elit tempus, sed tempus nunc rutrum. 
+    Donec finibus finibus eros at porttitor. Nulla pellentesque eros finibus aliquet lacinia.
+    Suspendisse dignissim sollicitudin leo, sed convallis neque sollicitudin `,
+    "photoCover":"https://neilpatel.com/wp-content/uploads/2019/05/word-image-10.jpeg"
+  }, 
+  {
+    "id":"4",
+    "title": "Android 14 é anunciado no Google I/O; saiba o que muda no seu celular",
+    "description": `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Integer ullamcorper mi sit amet velit feugiat aliquet tempus vitae velit. 
+    Ut pellentesque, nulla sed gravida bibendum, velit elit congue sem, eu faucibus 
+    lorem libero non erat. Sed accumsan nunc vel mi maximus euismod. Vivamus feugiat nunc lacus. 
+    Donec id velit quis nulla sodales varius vestibulum at elit. Etiam consequat nec tellus 
+    et convallis. Nulla sit amet erat in lectus pretium rhoncus ut nec tortor. 
+    Pellentesque pretium magna ac dignissim posuere. Fusce in nisl vehicula, luctus purus ut, 
+    varius nisl. Quisque vestibulum odio bibendum elit tempus, sed tempus nunc rutrum. 
+    Donec finibus finibus eros at porttitor. Nulla pellentesque eros finibus aliquet lacinia.
+    Suspendisse dignissim sollicitudin leo, sed convallis neque sollicitudin `,
+    "photoCover":"https://s2.glbimg.com/Hs9m6cI8eO3OTkmnqzRvm3rHg00=/0x0:1279x713/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/c/v/EgLp7nS8i6SME5LgBf6A/google-i-o-23-android-capa.png"
+  },         
 ]
